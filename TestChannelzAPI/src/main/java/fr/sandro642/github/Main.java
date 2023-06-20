4d8f88d184644014a58c770c518f8f7585ba6d80
Sandro642 <sandro33810@gmail.com>
2023-06-20 18:27:37 +0200

    Update

--- TestChannelzAPI/src/main/java/fr/sandro642/github/Main.java
+++ TestChannelzAPI/src/main/java/fr/sandro642/github/Main.java
@@ -1,4 +1,38 @@
 package fr.sandro642.github;
 
-public class Main {
+import fr.sandro642.github.ChannelzAPI.ChannelzAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public ChannelzAPI channelzAPI;
+
+    public static Player player;
+
+    public static Player player1;
+
+    public static int integer = 12;
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello World");
+        channelzAPI.setStatus(true);
+        channelzAPI.setChannels("Test");
+        channelzAPI.setSubChannels("test2");
+        channelzAPI.launchData();
+
+
+
+        // Set data
+
+        channelzAPI.setPlayer(player.toString());
+        channelzAPI.setInteger(12);
+
+
+        // getData
+
+        ChannelzAPI.Channelz().getPlayer(player1.toString());
+        ChannelzAPI.Channelz().getInteger(integer);
+    }
 }
--- TestGuiAPI/src/main/java/fr/sandro642/github/Main.java
+++ TestGuiAPI/src/main/java/fr/sandro642/github/Main.java
@@ -7,17 +7,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
-    public static GuiAPI guiAPI;
     public static Player player = Bukkit.getPlayer("Sandro642");
 
     @Override
     public void onEnable() {
-        System.out.println("Plugin enabled");
-        guiAPI.createinv("Test", 54);
-        guiAPI.setItem(Material.HEART_OF_THE_SEA, 2, "TestItem");
-        if (player != null) {
-            guiAPI.openInv(player, "Test");
+        System.out.println("Hello World");
+
+        GuiAPI.use().createInventory("Test", 54);
+        GuiAPI.use().addItem("Test", 9, Material.ANDESITE, "Salut tout le monde");
+
+
+        if (player.isOp()) {
+            GuiAPI.use().openInventory(player, "Test");
         }
     }
 }
