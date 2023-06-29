b746537d39063942e9a7c5d1e762c4cea907ea27
Sandro642 <sandro33810@gmail.com>
2023-06-30 01:24:03 +0200

    add for OauthAPI

---
+++ TestOauthAPI/src/main/java/fr/sandro642/github/Main.java
@@ -0,0 +1,27 @@
+package fr.sandro642.github;
+
+import fr.sandro642.github.OauthAPI.OauthAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+public class Main extends JavaPlugin {
+
+    public static Player player = Bukkit.getPlayer("Sandro642"); // Get player (for example) or Bukkit.getOnlinePlayers, etc...
+
+    @Override
+    public void onEnable() {
+        System.out.println( "Plugin enabled");
+
+        OauthAPI.getOauth().setStatus(true);
+        try {
+            OauthAPI.getOauth().launchData();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        OauthAPI.getOauth().oauthGui(player);
+    }
+}
